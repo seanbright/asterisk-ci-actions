96e74cf5a7b74e3ddc1beb45cbf9d2db4a8e1133
George Joseph <gjoseph@sangoma.com>
2023-04-12 07:49:58 -0600

    Add testsuite test action and fix issue in asterisk unit test

--- scripts/entrypointRunTestsuiteUnitTests.sh
+++ scripts/entrypointRunTestsuiteUnitTests.sh
@@ -4,10 +4,12 @@ set -e
 
 SCRIPT_DIR=${GITHUB_WORKSPACE}/$(basename ${GITHUB_ACTION_REPOSITORY})/scripts
 ASTERISK_DIR=${GITHUB_WORKSPACE}/$(basename ${INPUT_ASTERISK_REPO})
+TESTSUITE_DIR=${GITHUB_WORKSPACE}/$(basename ${INPUT_TESTSUITE_REPO})
 OUTPUT_DIR=${GITHUB_WORKSPACE}/cache/output
 
 [ ! -d ${SCRIPT_DIR} ] && { echo "::error::SCRIPT_DIR ${SCRIPT_DIR} not found" ; exit 1 ; } 
 [ ! -d ${ASTERISK_DIR} ] && { echo "::error::ASTERISK_DIR ${ASTERISK_DIR} not found" ; exit 1 ; } 
+[ ! -d ${TESTSUITE_DIR} ] && { echo "::error::TESTSUITE_DIR ${TESTSUITE_DIR} not found" ; exit 1 ; } 
 [ ! -d ${OUTPUT_DIR} ] && { echo "::error::OUTPUT_DIR ${OUTPUT_DIR} not found" ; exit 1 ; } 
 
 cd ${ASTERISK_DIR}
@@ -16,8 +18,5 @@ ${SCRIPT_DIR}/installAsterisk.sh --github --uninstall-all \
   --branch-name=${INPUT_BASE_BRANCH} --user-group=asteriskci:users \
   --output-dir=${OUTPUT_DIR}
 
-${SCRIPT_DIR}/runAsteriskUnittests.sh --no-expect --github \
-  --user-group=asteriskci:users \
-  --output-dir=${OUTPUT_DIR} \
-  --output-xml=${OUTPUT_DIR}/unittests-results.xml \
-  --unittest-command="${INPUT_UNITTEST_COMMAND}"
+cd ${TESTSUITE_DIR}
+./runInVenv.sh ${INPUT_UNITTEST_COMMAND}
