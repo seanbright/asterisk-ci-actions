db09bec0aedb718c4d0a309c4b8b5bce611fbe10
George Joseph <gjoseph@sangoma.com>
2023-04-25 09:38:03 -0600

    test docker action

---
+++ TestDockerAction/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+set -x
+if [ "${INPUT_TEST_TYPE}" == "pass_fail" ] ; then
+	echo "Exiting with RC 1 (forced)"
+	echo "result=failure" >> $GITHUB_OUTPUT
+	exit 1
+fi
+echo "Exiting with RC 0 (forced)"
+echo "result=success" >> $GITHUB_OUTPUT
+exit 0
