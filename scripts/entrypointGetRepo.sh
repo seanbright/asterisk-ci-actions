5920f704075db1e480f48bc0b5e17d2038999d0e
George Joseph <gjoseph@sangoma.com>
2023-04-25 07:30:43 -0600

    debugging failures

--- scripts/entrypointGetRepo.sh
+++ scripts/entrypointGetRepo.sh
@@ -4,8 +4,12 @@ set -e
 
 SCRIPT_DIR=${GITHUB_WORKSPACE}/$(basename ${GITHUB_ACTION_REPOSITORY})/scripts
 REPO_DIR=${GITHUB_WORKSPACE}/$(basename ${INPUT_REPO})
+OUTPUT_DIR=${GITHUB_WORKSPACE}/${INPUT_CACHE_DIR}/output
 
 mkdir -p ${REPO_DIR}
+mkdir -p ${OUTPUT_DIR}
+
+cd ${GITHUB_WORKSPACE}
 ${SCRIPT_DIR}/checkoutRepo.sh --repo=${INPUT_REPO} \
 	--base-branch=${INPUT_BASE_BRANCH} --is-cherry-pick=${INPUT_IS_CHERRY_PICK} \
 	--pr-number=${INPUT_PR_NUMBER} --destination=${REPO_DIR}
@@ -13,9 +17,6 @@ ${SCRIPT_DIR}/checkoutRepo.sh --repo=${INPUT_REPO} \
 cd ${REPO_DIR}
 
 if [ "x${INPUT_BUILD_SCRIPT}" != "x" ] ; then
-	OUTPUT_DIR=${GITHUB_WORKSPACE}/${INPUT_CACHE_DIR}/output
-	mkdir -p ${OUTPUT_DIR}
-
 	${SCRIPT_DIR}/${INPUT_BUILD_SCRIPT} --github --branch-name=${INPUT_BASE_BRANCH} \
 		--modules-blacklist="${INPUT_MODULES_BLACKLIST// /}" \
 		--output-dir=${OUTPUT_DIR}
