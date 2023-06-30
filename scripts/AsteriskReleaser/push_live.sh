0074980cfbc747cb715f28f55f3b2a9fede5b377
George Joseph <gjoseph@sangoma.com>
2023-06-30 08:38:21 -0600

    push_live: Break release creation into multiple 'gh' calls

--- scripts/AsteriskReleaser/push_live.sh
+++ scripts/AsteriskReleaser/push_live.sh
@@ -13,28 +13,46 @@ ${DEBUG} && declare -p end_tag
 
 debug "Pushing Asterisk Release ${END_TAG} live"
 cd "${SRC_REPO}"
-RC=0
-$ECHO_CMD gh release create ${END_TAG} \
+
+# Creating the release with all the assets seems to
+# fail much of the time so we'll create first, then
+# upload the assets one at a time.
+
+declare -i RC=0
+set -x
+gh release create ${END_TAG} \
 	--verify-tag \
 	$( [ "${end_tag[release_type]}" != "ga" ] && echo "--prerelease" ) \
 	--notes-file ${DST_DIR}/email_announcement.md \
-	--target ${end_tag[branch]} -t "Asterisk Release ${END_TAG}" \
-	${DST_DIR}/asterisk-${END_TAG}.* \
-	${DST_DIR}/ChangeLog-${END_TAG}.md \
-	${DST_DIR}/README-${END_TAG}.md || RC=1
-if [ $RC -eq 1 ] ; then
+	--target ${end_tag[branch]} -t "Asterisk Release ${END_TAG}" || RC=1
+
+if [ $RC -ne 0 ] ; then
+	# Try again
+	echo "First attempt failed.  Trying again"
 	RC=0
-	$ECHO_CMD gh release create ${END_TAG} \
+	gh release create ${END_TAG} \
 		--verify-tag \
 		$( [ "${end_tag[release_type]}" != "ga" ] && echo "--prerelease" ) \
-		--notes-file ${DST_DIR}/release_notes.md \
-		--target ${end_tag[branch]} -t "Asterisk Release ${END_TAG}" \
-		${DST_DIR}/asterisk-${END_TAG}.* \
-		${DST_DIR}/ChangeLog-${END_TAG}.md \
-		${DST_DIR}/README-${END_TAG}.md || RC=1
+		--notes-file ${DST_DIR}/email_announcement.md \
+		--target ${end_tag[branch]} -t "Asterisk Release ${END_TAG}" || RC=1
 fi
 
-[ $RC -ne 0 ] && bail "Unable to create GitHub release!!"
+if [ $RC -ne 0 ] ; then
+	bail "Unable to create release!"
+fi
+
+RC=0
+for f in ${DST_DIR}/asterisk-${END_TAG}* \
+	${DST_DIR}/ChangeLog-${END_TAG}.md \
+	${DST_DIR}/README-${END_TAG}.md ; do
+	gh release upload ${END_TAG} --clobber $f || \
+		{ echo "Retrying..." ; gh release upload ${END_TAG} --clobber $f ; } || { RC=1 ; break ; }
+done
+
+if [ $RC -ne 0 ] ; then
+	gh release delete --yes ${END_TAG}
+	bail "Unable to attach artifacts.  Release deleted!!  You need to clean up the release branch."
+fi
 
 $PUSH_TARBALLS || exit 0
 
