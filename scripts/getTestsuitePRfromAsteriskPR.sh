0d6772c0c58d32514e69b4149ec235234dfe99a7
George Joseph <gjoseph@sangoma.com>
2025-02-06 11:39:49 -0700

    getTestsuitePRfromAsteriskPR.sh: Bring up to date

--- scripts/getTestsuitePRfromAsteriskPR.sh
+++ scripts/getTestsuitePRfromAsteriskPR.sh
@@ -4,21 +4,29 @@ SCRIPT_DIR=$(dirname $(readlink -fn $0))
 PROGNAME=$(basename $(readlink -fn $0))
 . ${SCRIPT_DIR}/ci.functions
 
-[ -z "${GH_TOKEN}" ] && { echo "${PROGNAME}: GH_TOKEN must be passed as an environment variable" >&2 ; exit 1 ; }
-[ -z "${REPO}" ] && { echo "${PROGNAME}: --repo=<owner/repo> required" >&2 ; exit 1 ; }
-[ -z "${PR_NUMBER}" ] && { echo "${PROGNAME}: --pr-number=<asterisk pr number> required"  >&2 ; exit 1 ; }
-[ -z "${TESTSUITE_PR_REGEX}" ] && { echo "${PROGNAME}: --testsuite-pr-regex=<testsuite pr regex> required" >&2 ; exit 1 ; }
+for v in REPO PR_NUMBER TESTSUITE_PR_REGEX ; do
+	assert_env_variable $v || exit 1
+done
 
 jq_exp=".[].body | match(\"${TESTSUITE_PR_REGEX}\"; \"g\") | .captures[0].string"
 testsuite_pr=$(gh api /repos/${REPO}/issues/${PR_NUMBER}/comments \
-  --jq "$jq_exp") || \
-  { echo "${PROGNAME}: Unable to retrieve comments for /repos/${REPO}/issues/${PR_NUMBER}" >&2 ; exit 1 ; }
+	--jq "$jq_exp") || \
+	{ debug_out "::error::Unable to retrieve comments for /repos/${REPO}/issues/${PR_NUMBER}" ; exit 1 ; }
 
-if [ -n "${testsuite_pr}" ] ; then
-  echo "${PROGNAME}: Testsuite PR: ${testsuite_pr}" >&2
-  echo ${testsuite_pr}
-else
-  echo "${PROGNAME}: No Testsuite PR found (OK)" >&2
+if [ -z "${testsuite_pr}" ] ; then
+	debug_out "No Testsuite PR found (OK)"
+	exit 0
 fi
+
+debug_out "Testsuite PR: ${testsuite_pr}"
+
+if [ -n "$GITHUB_ENV" ] ; then
+	echo "TESTSUITE_TEST_PR=${testsuite_pr}" >> ${GITHUB_ENV}
+fi
+if [ -n "$GITHUB_OUTPUT" ] ; then
+	echo "TESTSUITE_TEST_PR=${testsuite_pr}" >> ${GITHUB_OUTPUT}
+fi
+
+echo ${testsuite_pr}
 
 exit 0
